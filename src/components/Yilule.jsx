39e2635616c4c552468097ef297c17c5c0044ad2
sheldon yao <sheldon yao>
2017-05-05 12:46:03 +0800

    tes2

---
+++ src/components/Yilule.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Yilule = props => {
+    let { a } = props;
+    a();
+    return (
+        <div>
+
+            一路乐
+        </div>
+    );
+};
+
+export default Yilule;
